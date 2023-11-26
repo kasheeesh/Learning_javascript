b68678fd5de0d3441fa8f7b2a2f1dfca4ff2d55c
kasheesh <kashishbhurani@gmail.com>
2023-11-26 18:10:35 +0530

    Learnt about variables and data types

--- 1-variables.js
+++ 1-variables.js
@@ -1,1 +1,22 @@
-//node is used to runtime environment used to run javascript outside the browser
+//1. node is used to runtime environment used to run javascript outside the browser
+//Variables and Constants
+
+const accountId = 12;
+let accountEmail = "abc@gmail.com";
+var accountPassword = "hi123";
+accountCity = "Jaipur";
+accountid = 2;
+let accountstate; //This will be undefined if console logged
+
+
+//variables declared with the keyword const cannot be changed
+
+console.table([accountEmail, accountPassword, accountid])
+/*
+Prefer not to use var because of issue in block scope and functional scope
+js is a safe language, so this is also possible 
+accountCity = "Jaipur";
+
+ */
+
+
---
+++ 2-datatypes.js
@@ -0,0 +1,17 @@
+"use strict" //treat all JS code as newer version
+let name = "Kashish"
+//alert("hi"); This will not work since we're using node.js
+//code readability is very important
+/* 
+number
+bigInt
+String = ""
+boolean
+undefined => declared the variable but have not assigned value
+null => empty value
+symbol => used for uniqueness
+object
+ */
+console.log(typeof name)
+console.log(typeof null)
+console.log(typeof undefined)
