aa2911576999e83770bf08b42e011e56258d33e4
kasheesh <kashishbhurani@gmail.com>
2023-11-26 22:57:45 +0530

    Learnt datatype conversions

---
+++ 3-conversion.js
@@ -0,0 +1,15 @@
+let age = "33";
+console.log(typeof age);
+let ValueInNumber = Number(age);
+console.log(typeof ValueInNumber);
+console.log(ValueInNumber);
+let score = null;
+/* 
+"33" => 33
+"33abd" => NaN
+true => 1
+false => 0
+null => 0
+undefined => NaN
+"" => false (incase of converting in boolean)
+*/
