37f1d423ccc6ac45999fcbcf4c94ceb5cfc0aa4e
kasheesh <kashishbhurani@gmail.com>
2023-11-27 15:24:06 +0530

    Learnt datatype ops

---
+++ 4-conversionOperations.js
@@ -0,0 +1,15 @@
+let value = 3
+let negvalue = -value;
+console.log(negvalue)
+// console.log(2+2);
+// console.log(2-2);
+//all basic math operations are the same like in other languages
+let str1 = "hello";
+let str2 = " kashish";
+let str3 = str1 + str2;
+console.log(str3);
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1 + 2 + "2"); //32
+console.log("1" + 2 + 2); //122
+
---
+++ 5-comparison.js
@@ -0,0 +1,5 @@
+console.log("2" > 1);
+console.log("02" > 1);
+
+//strict check ===
+//clean code and code readablitiy most important
